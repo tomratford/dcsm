fa989efe3e90ba913a6fbf4b3767c6111da31326
Tom Ratford <tomratford@gmail.com>
2024-08-23 10:32:18 +0100

    fix: model fitting procedure, infinite loop occuring

--- simulations/50203_model_fitting.R
+++ simulations/50203_model_fitting.R
@@ -9,6 +9,7 @@ weib_mod <- weib.fit(dat)
 
 # Royston-Parmar model
 # Forward fit from 0 knots
+cat("Fitting Royston Model\n")
 rknots <- c(0, 0, 0)
 fit_royston <- \(ks) {
   v <- try(royston_parmar.fit(dat, ks[1], ks[2], ks[3]))
@@ -21,7 +22,6 @@ royston_mod <- fit_royston(rknots)
 history_royston_mods <- list(royston_mod)
 history_rknots <- list(rknots)
 history_pvals <- c()
-print("Model fitting starts now")
 while (TRUE) {
   possible_mods <- vector("list", 3)
   pvals <- vector("numeric", 3)
@@ -30,6 +30,9 @@ while (TRUE) {
   possible_rknots[[1]] <- rknots + c(1, 0, 0)
   possible_rknots[[2]] <- rknots + c(0, 1, 0)
   possible_rknots[[3]] <- rknots + c(0, 0, 1)
+  # restrict to a maximum of three knots
+  possible_rknots <- lapply(possible_rknots,
+                            \(rk) mapply(\(x,y) min(x,y), rk, c(3,3,3)))
 
   possible_mods <- parallel::mclapply(possible_rknots, fit_royston, mc.cores = 3)
 
@@ -47,10 +50,11 @@ while (TRUE) {
     break
   }
 }
-
+cat("Royston Model done\n")
 
 # Penalised I-spline model
 # Forward fitting with AIC
+cat("Fitting Joly Model\n")
 jknots <- c(0, 0, 0)
 fit_joly <- \(ks) {
   v <- try(joly.fit(dat, ks[1], ks[2], ks[3], 100, 100, 100),T)
@@ -68,9 +72,9 @@ while (TRUE) {
   AIC <- vector("numeric", 3)
   possible_jknots <- vector("list", 3)
 
-  possible_jknots[[1]] <- jknots - c(1, 0, 0)
-  possible_jknots[[2]] <- jknots - c(0, 1, 0)
-  possible_jknots[[3]] <- jknots - c(0, 0, 1)
+  possible_jknots[[1]] <- jknots + c(1, 0, 0)
+  possible_jknots[[2]] <- jknots + c(0, 1, 0)
+  possible_jknots[[3]] <- jknots + c(0, 0, 1)
 
   possible_mods <- parallel::mclapply(possible_rknots, fit_royston, mc.cores = 3)
 
@@ -87,6 +91,7 @@ while (TRUE) {
     break
   }
 }
+cat("Joly Model done\n")
 
 save(
   list = c("weib_mod", "royston_mod", "joly_mod"),
